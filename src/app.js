f70f28299e5b4884e2aa0ab6036d2faca75b2f53
Lisi Linhart <e.w.l@live.at>
2016-12-21 16:20:04 +0100

    App Class added

--- src/app.js
+++ src/app.js
@@ -1,32 +1,19 @@
 import * as d3 from "d3";
 
-const margin = {top: 40, bottom: 10, left: 120, right: 20};
-const width = 800 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
-// Creates sources <svg> element
-const svg = d3.select('#app').append('svg')
-            .attr('width', width+margin.left+margin.right)
-            .attr('height', height+margin.top+margin.bottom);
-// Group used to enforce margin
-const data = [66.38, 21.51, 23.37, 34.17, 36.21];
+class Application {
+    constructor(options) {
+        console.log('App Init');
+    }
 
-const g = svg.append('g')
-            .attr('transform', `translate(${margin.left},${margin.top})`);
+    init(options) {
+      const margin = {top: 40, bottom: 10, left: 120, right: 20};
+      const width = 800 - margin.left - margin.right;
+      const height = 600 - margin.top - margin.bottom;
+      // Creates sources <svg> element
+      const svg = d3.select('#app').append('svg')
+                  .attr('width', width+margin.left+margin.right)
+                  .attr('height', height+margin.top+margin.bottom);
+    }
+}
 
-
-
-// Global variable for all data
-const bar_height = 50;
-const rects = g.selectAll('rects').data(data)
-
-const rect_enter = rects.enter().append('rect');
-rect_enter.append('title');
-
-rects.merge(rect_enter)
-  .attr('height', bar_height)
-  .attr('width', (d,i) => d*7)
-  .attr('fill', '#ff0000')
-  .attr('y', (d,i) => i*(bar_height + 5))
-  .select('title').text((d) => d);
-
-rects.exit().remove();
+window.Application = new Application();
---
+++ src/components/BarChart.js
@@ -0,0 +1,5 @@
+export default class BarChart {
+  constructor() {
+
+  }
+}
