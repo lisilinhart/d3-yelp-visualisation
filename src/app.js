8e022dd7e234ca52822fcd7076f05220faa9d2f6
Lisi Linhart <e.w.l@live.at>
2016-12-21 16:33:43 +0100

    d3 import

--- src/app.js
+++ src/app.js
@@ -5,15 +5,9 @@ class Application {
     console.log('App Init');
   }
 
-  init (options) {
-    const margin = {top: 40, bottom: 10, left: 120, right: 20};
-    const width = 800 - margin.left - margin.right;
-    const height = 600 - margin.top - margin.bottom;
-      // Creates sources <svg> element
-      const svg = d3.select('#app').append('svg')
-      .attr('width', width+margin.left+margin.right)
-      .attr('height', height+margin.top+margin.bottom);
-    }
+  init(options) {
+
+  }
 }
 
 window.Application = new Application();
