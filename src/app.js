1124496a27a86cd4254005e777728c4aed80a472
Lisi Linhart <e.w.l@live.at>
2016-12-21 16:24:18 +0100

    Linter added

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-import * as d3 from "d3";
+import * as d3 from 'd3';
 
 class Application {
-    constructor(options) {
-        console.log('App Init');
-    }
+  constructor(options) {
+    console.log('App Init');
+  }
 
-    init(options) {
-      const margin = {top: 40, bottom: 10, left: 120, right: 20};
-      const width = 800 - margin.left - margin.right;
-      const height = 600 - margin.top - margin.bottom;
+  init (options) {
+    const margin = {top: 40, bottom: 10, left: 120, right: 20};
+    const width = 800 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
       // Creates sources <svg> element
       const svg = d3.select('#app').append('svg')
-                  .attr('width', width+margin.left+margin.right)
-                  .attr('height', height+margin.top+margin.bottom);
+      .attr('width', width+margin.left+margin.right)
+      .attr('height', height+margin.top+margin.bottom);
     }
 }
 
