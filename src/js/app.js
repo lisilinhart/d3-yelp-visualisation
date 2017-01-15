1dac11d48babcc90404dece29997fac89b5ebcb4
Marcel Freinbichler <marcel@freinbichler.me>
2017-01-15 15:09:38 +0100

    add real data to word clouds

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,10 @@
-import * as d3 from 'd3';
 import TinyEmitter from 'tiny-emitter';
-
 import Navigation from './components/navigation';
-
 import BarChart from './components/bar-chart';
 import WordCloud from './components/word-cloud';
 
 class Application {
-  constructor(options) {
+  constructor() {
     console.log('--------- App Init ---------');
     window.emitter = new TinyEmitter();
 
@@ -15,7 +12,7 @@ class Application {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
     this.container = document.querySelector('#app');
-    this.colors = ['#364958', '#3B6064', '#55828B', '#87BBA2', '#C9E4CA'];
+    this.colors = ['#C9E4CA', '#87BBA2', '#55828B', '#3B6064', '#364958'];
     // this.colors = ['#220901', '#621708', '#941B0C', '#BC3908', '#F6AA1C'];
     // this.colors = ['#0D1B2A', '#1B263B', '#415A77', '#778DA9', '#656D77'];
 
@@ -32,8 +29,18 @@ class Application {
     });
 
     this.wordCloud = new WordCloud({
-      file: 'edinburgh_review',
-      container: this.container.querySelector('.word-cloud'),
+      city: 'edinburgh',
+      reviewStars: 5,
+      container: this.container.querySelector('.word-cloud-positive'),
+      colors: this.colors,
+      width: this.width / 2,
+      height: this.height / 2,
+    });
+
+    this.wordCloud = new WordCloud({
+      city: 'edinburgh',
+      reviewStars: 1,
+      container: this.container.querySelector('.word-cloud-negative'),
       colors: this.colors,
       width: this.width / 2,
       height: this.height / 2,
--- src/js/components/word-cloud.js
+++ src/js/components/word-cloud.js
@@ -2,57 +2,85 @@ import * as d3 from 'd3';
 import cloud from 'd3-cloud';
 
 export default class WordCloud {
-  constructor({ data, container, width, height, city, colors }) {
+  constructor({ container, width, height, city, reviewStars, colors }) {
     console.log('----- Word Cloud Init -----');
-    this.file = `../data/${city}_review_words.csv`;
-    this.data = data;
+    this.file = `../data/review_words_${city}_${reviewStars}_star.json`;
+    this.reviewStars = reviewStars;
     this.colors = colors;
     this.container = container;
     this.width = width;
     this.height = height;
 
+    window.emitter.on('updateCharts', this.updateData.bind(this));
     this.init();
   }
 
   init() {
-    this.frequency_list = [{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10},{"text":"electricity","size":15},{"text":"movement","size":10},{"text":"relation","size":5},{"text":"things","size":10},{"text":"force","size":5},{"text":"ad","size":5},{"text":"energy","size":85},{"text":"living","size":5},{"text":"nonliving","size":5},{"text":"laws","size":15},{"text":"speed","size":45},{"text":"velocity","size":30},{"text":"define","size":5},{"text":"constraints","size":5},{"text":"universe","size":10},{"text":"physics","size":120},{"text":"describing","size":5},{"text":"matter","size":90},{"text":"physics-the","size":5},{"text":"world","size":10},{"text":"works","size":10},{"text":"science","size":70},{"text":"interactions","size":30},{"text":"studies","size":5},{"text":"properties","size":45},{"text":"nature","size":40},{"text":"branch","size":30},{"text":"concerned","size":25},{"text":"source","size":40},{"text":"google","size":10},{"text":"defintions","size":5},{"text":"two","size":15},{"text":"grouped","size":15},{"text":"traditional","size":15},{"text":"fields","size":15},{"text":"acoustics","size":15},{"text":"optics","size":15},{"text":"mechanics","size":20},{"text":"thermodynamics","size":15},{"text":"electromagnetism","size":15},{"text":"modern","size":15},{"text":"extensions","size":15},{"text":"thefreedictionary","size":15},{"text":"interaction","size":15},{"text":"org","size":25},{"text":"answers","size":5},{"text":"natural","size":15},{"text":"objects","size":5},{"text":"treats","size":10},{"text":"acting","size":5},{"text":"department","size":5},{"text":"gravitation","size":5},{"text":"heat","size":10},{"text":"light","size":10},{"text":"magnetism","size":10},{"text":"modify","size":5},{"text":"general","size":10},{"text":"bodies","size":5},{"text":"philosophy","size":5},{"text":"brainyquote","size":5},{"text":"words","size":5},{"text":"ph","size":5},{"text":"html","size":5},{"text":"lrl","size":5},{"text":"zgzmeylfwuy","size":5},{"text":"subject","size":5},{"text":"distinguished","size":5},{"text":"chemistry","size":5},{"text":"biology","size":5},{"text":"includes","size":5},{"text":"radiation","size":5},{"text":"sound","size":5},{"text":"structure","size":5},{"text":"atoms","size":5},{"text":"including","size":10},{"text":"atomic","size":10},{"text":"nuclear","size":10},{"text":"cryogenics","size":10},{"text":"solid-state","size":10},{"text":"particle","size":10},{"text":"plasma","size":10},{"text":"deals","size":5},{"text":"merriam-webster","size":5},{"text":"dictionary","size":10},{"text":"analysis","size":5},{"text":"conducted","size":5},{"text":"order","size":5},{"text":"understand","size":5},{"text":"behaves","size":5},{"text":"en","size":5},{"text":"wikipedia","size":5},{"text":"wiki","size":5},{"text":"physics-","size":5},{"text":"physical","size":5},{"text":"behaviour","size":5},{"text":"collinsdictionary","size":5},{"text":"english","size":5},{"text":"time","size":35},{"text":"distance","size":35},{"text":"wheels","size":5},{"text":"revelations","size":5},{"text":"minute","size":5},{"text":"acceleration","size":20},{"text":"torque","size":5},{"text":"wheel","size":5},{"text":"rotations","size":5},{"text":"resistance","size":5},{"text":"momentum","size":5},{"text":"measure","size":10},{"text":"direction","size":10},{"text":"car","size":5},{"text":"add","size":5},{"text":"traveled","size":5},{"text":"weight","size":5},{"text":"electrical","size":5},{"text":"power","size":5}];
+    d3.json(this.file, (json) => {
+      this.words = json;
+      this.processWords();
+    });
+  }
 
-    const maxValue = d3.max(this.frequency_list, d => d.size);
+  updateData(city) {
+    this.file = `../data/review_words_${city}_${this.reviewStars}_star.json`;
+    d3.select(this.container).selectAll('svg').remove();
+    this.init();
+  }
+
+  processWords() {
+    const maxValue = d3.max(this.words, d => d.size);
+    const minValue = d3.min(this.words, d => d.size);
+
     this.colorScale = d3.scaleLinear()
-      .domain([0, maxValue * 0.05, maxValue * 0.1, maxValue * 0.2, maxValue])
+      .domain([0, 30, 50, 80, 100])
       .interpolate(d3.interpolateHcl)
-      .range([d3.rgb(this.colors[0]),
+      .range([
+        d3.rgb(this.colors[0]),
         d3.rgb(this.colors[1]),
         d3.rgb(this.colors[2]),
         d3.rgb(this.colors[3]),
         d3.rgb(this.colors[4]),
       ]);
 
-    this.chart = cloud().size([this.width - 50, this.height - 50])
-      .words(this.frequency_list)
+    this.opacityScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([1.0, 0.2]);
+
+    this.fontScale = d3.scaleLinear()
+      .domain([minValue, maxValue])
+      .range([10, 100]);
+
+    const fontScale = this.fontScale;
+    cloud().size([this.width - 50, this.height - 50])
+      .words(this.words)
       .rotate(0)
-      .fontSize(d => d.size)
+      .fontSize(d => fontScale(d.size))
+      .padding(5)
       .on('end', this.createCloud.bind(this))
       .start();
   }
 
   createCloud(words) {
     const colorScale = this.colorScale;
-    d3.select(this.container)
+    const opacityScale = this.opacityScale;
+
+    this.chart = d3.select(this.container)
       .append('svg')
       .attr('width', this.width)
       .attr('height', this.height)
       .attr('class', 'wordcloud')
       .append('g')
+      .attr('class', 'wordgroup')
       .attr('transform', `translate(${this.height / 1.3}, ${this.height / 2})`)
       .selectAll('text')
       .data(words)
-      .enter().append('text')
-      .style('font-size', d => d.size + 'px')
-      .style('fill', (d, i) => colorScale(i))
-      .attr('transform', (d) => {
-        return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
-      })
+      .enter()
+      .append('text')
+      .style('font-size', d => `${d.size}px`)
+      .style('fill', d => colorScale(d.size))
+      .style('opacity', d => opacityScale(d.size))
+      .attr('transform', d => `translate(${[d.x, d.y]}) rotate(${d.rotate})`)
       .text(d => d.text);
   }
 }
